a27827d1e68601b1475bd601eea4bf54504583f5
hotwh-et <ethan10.wong@gmail.com>
2025-12-03 13:18:08 -0800

    Complete assignment 6.2

--- 6.2/search.py
+++ 6.2/search.py
@@ -1,4 +1,24 @@
-file = open("6.2/spotify.csv")
+def selection_sort(comparing_list, output_list=None):
+	if output_list == None:
+		output_list = comparing_list
+
+	sorted_comparing_list = comparing_list.copy()
+	sorted_output_list = output_list.copy()
+	for i in range(len(sorted_comparing_list) - 1):
+		current_minimum = sorted_comparing_list[i]
+		current_minimum_index = i
+		for j in range(len(sorted_comparing_list) - 1 - i):
+			if sorted_comparing_list[j + i + 1] < current_minimum:
+				current_minimum = sorted_comparing_list[j + i + 1]
+				current_minimum_index = j + i + 1
+
+		popped = sorted_comparing_list.pop(current_minimum_index)
+		sorted_comparing_list.insert(i, popped)
+		sorted_output_list.insert(i, sorted_output_list.pop(current_minimum_index))
+
+	return sorted_output_list
+
+file = open("6.2/spotify.csv", encoding="utf-8")
 junk = file.readline()
 
 drake_data = []
@@ -11,6 +31,8 @@ for line in file:
 	
 	if artist == "Drake":
 		drake_data.append([danceability, songtitle, artist])
+	
+	drake_data = selection_sort(drake_data)[::-1][:5]
 
 print("Dance score \tSong")
 for item in drake_data:
